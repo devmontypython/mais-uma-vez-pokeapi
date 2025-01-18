0cfbbe03b6af7c9d0bd6b81756dac491bc621623
devmontypython <devmontypython>
2025-01-18 02:34:00 -0300

    2) Depois da resposta OK, acrescentamos uma instrução if. Se nossa resposta objeto tiver um atributo de status de código para ler e  se esse status for igual a 200, significa que a resposta está OK, e nós escrevemos: "Data retrieve", senão escrevemos: "Failed to retrieve data {response.staus_code}". Então rodamos.

--- pokeapi.py
+++ pokeapi.py
@@ -7,5 +7,10 @@ def get_pokemon_info(name):
     response = requests.get(url)
     print(response)
 
+    if response.status_code == 200:
+        print("Data retrieved!")
+    else:
+        print(f"Failed to retrieve data {response.status_code}")
+
 pokemon_name = "pikachu"
 get_pokemon_info(pokemon_name)
